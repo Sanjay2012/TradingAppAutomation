c85768f27562241dc974e3032a6e8c895d6837b8
Shivansh <sanjaywaware04@gmail.com>
2022-04-08 20:29:29 +0530

    upload project

---
+++ src/test/java/com/zerodha/kite/pages/KiteHomePage.java
@@ -0,0 +1,64 @@
+package com.zerodha.kite.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class KiteHomePage {
+	
+	//SoftAssert soft=new SoftAssert();
+	// declaration
+				@FindBy(xpath = "//*[@class=\"avatar\"]") 
+					private WebElement profileName;
+				
+				@FindBy(xpath = "//*[@class=\"user-id\"]")
+				private WebElement menuButton;
+				
+				@FindBy(xpath = "//*[@class=\"icon icon-exit\"]")
+				private WebElement logoutButton;
+
+				WebDriver driver;
+
+				
+				// initilization
+				
+				public KiteHomePage (WebDriver driver) {
+					
+					PageFactory.initElements(driver, this);
+					
+					
+				}
+				
+				public String
+				veryfyKiteHomePageProfileName( ) {
+					String actual = profileName.getText();
+					return actual;
+					
+				}
+				
+				public boolean 
+				verifyProfileMenuButton() {
+					boolean status = menuButton.isEnabled();
+					return status;
+					
+				}
+				
+				public void
+				clickOnProfileMenuButton() {
+					menuButton.click();
+				}
+				
+				public boolean
+				verifyLogoutButton() {
+					boolean status = logoutButton.isEnabled();
+					return status;
+
+				}
+				
+				public void
+				clickOnLogoutButton() {
+					logoutButton.click();
+				}
+
+}
